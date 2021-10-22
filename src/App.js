704ddfd44704fcd17d18cdae865415f097275c10
hrmvlg <hrmvlg@mail.ru>
2021-10-22 17:25:41 +0300

    added props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
-import List from "./components/List";
+import List from "./components/List/index";
 
 function App(){
   return(
     <div className="todo">
       <div className="todo__sidebar">
-        <List/>
+        <List items={[
+          {
+            icon: <i className="fa fa-list-ul" />,
+            name: 'Все задачи',
+            active: true,
+          },
+        ]}
+        />
+        <List items={[
+          {
+            color: 'red' ,
+            name: 'Пук',
+          },
+          {
+            color: 'green' ,
+            name: 'Среньк',
+          },
+          {
+            color: 'blue' ,
+            name: 'Как',
+          },
+        ]}
+        />
       </div>
       <div className="todo__tasks"></div>
     </div>
